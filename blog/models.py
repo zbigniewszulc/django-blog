5b29a6b9acfbcb24f665f84afad5f87361742c37
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-06-27 22:49:05 +0000

    add comment model

--- blog/models.py
+++ blog/models.py
@@ -8,10 +8,27 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="blog_posts"
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     xcerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, 
+        on_delete=models.CASCADE, 
+        related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="commenter"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
