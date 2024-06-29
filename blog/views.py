2bfe9dd699531bd1c3f9149f35e04c0509d396ec
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-06-29 18:45:44 +0000

    intergrate Summernote package and personalise admin dashboard

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,7 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.all()
+    # queryset = Post.objects.all().filter(status=1)
+    # queryset = Post.objects.all().order_by("-created_on")
+    # queryset = Post.objects.filter(author=2)
     template_name = "post_list.html"
