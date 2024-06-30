b656e35781d69412881d5320df8b7180605366dd
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-06-30 20:09:58 +0000

    change query in blog view (class PostList) to filter query set to post with status "published"

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,9 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    # queryset = Post.objects.all()
     # model = Post
-    # queryset = Post.objects.all().filter(status=1)
+    queryset = Post.objects.all().filter(status=1)
     # queryset = Post.objects.all().order_by("-created_on")
     # queryset = Post.objects.filter(author=2)
     # template_name = "post_list.html"
@@ -33,5 +33,5 @@ def post_detail(request, slug):
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {"post": post}, # pass data from view to template via context dictionary
     )
