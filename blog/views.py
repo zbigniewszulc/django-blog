50f05bce636b1afcdf073fc1ce99cbd48be06366
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-06-30 10:06:03 +0000

    add new static folder and set base template

--- blog/views.py
+++ blog/views.py
@@ -8,4 +8,6 @@ class PostList(generic.ListView):
     # queryset = Post.objects.all().filter(status=1)
     # queryset = Post.objects.all().order_by("-created_on")
     # queryset = Post.objects.filter(author=2)
-    template_name = "post_list.html"
+    # template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
