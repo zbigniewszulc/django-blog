0dfdde4ccfc5ab455aa05570e81f16e2ac0a972f
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-06-30 00:13:44 +0000

    correct Post model excerp field name

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    xcerpt = models.TextField(blank=True)
+    excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
