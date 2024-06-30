3b886de57d7a7196fcb9577c442a713fff4c2c10
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-06-30 18:00:48 +0000

    add fucntion based post_detail view which helps to display individual post

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.all()
+    # model = Post
     # queryset = Post.objects.all().filter(status=1)
     # queryset = Post.objects.all().order_by("-created_on")
     # queryset = Post.objects.filter(author=2)
     # template_name = "post_list.html"
     template_name = "blog/index.html"
     paginate_by = 6
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
