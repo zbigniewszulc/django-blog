d04b0060a29b601b07c79f06db392cb2aefe24bf
Zbigniew Szulc <zbigniewszulc@gmail.com>
2025-02-07 20:29:28 +0000

    adjust local settings and update requirements

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 # DEBUG = True
 
-ALLOWED_HOSTS = ['8000-zbigniewszul-djangoblog-cz644t9jxdr.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = [
+    '8000-zbigniewszul-djangoblog-cz644t9jxdr.ws.codeinstitute-ide.net', 
+    '.herokuapp.com', 
+    '127.0.0.1'
+]
 
 
 # Application definition
